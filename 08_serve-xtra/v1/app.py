5f60702bc269133e64c4b9033961cebcc47d5c2d
anthonyqsun <qsun30@stuy.edu>
2022-10-11 09:20:15 -0400

    fix random color job

--- 08_serve-xtra/v1/app.py
+++ 08_serve-xtra/v1/app.py
@@ -18,19 +18,17 @@ OPS SUMMARY:
 * We made the information more presentable by wrapping it with HTML
 
 """
-import random
+
 import numbercruncher
 from flask import Flask
 app = Flask(__name__)
 
-jobDict = numbercruncher.generate_dict("occupations.csv")
-
 @app.route("/")
 def disp_random_occ():
+    jobDict = numbercruncher.generate_dict("occupations.csv") 
     s = ""
-    job = str(numbercruncher.give_weighted_job(jobDict)).lower()
-    s += "<h1>10 items or fewer express checkout lane </h1> <h2> Daniel Liu, Anthony Sun, Jasmine Yuen </h2>"
-    s += f"<p>Your best bet is the <i> <a style = \"color: hsl({random.randrange(0,256)},90%,30%)\" href=\"https://www.google.com/search?q=how+to+apply+to+ {job.replace(' ','+')}\"> {job}</a></i> job </p>"
+    s += "<h1 >10 items or fewer express checkout lane </h1> <h2> Daniel Liu, Anthony Sun, Jasmine Yuen </h2>"
+    s += "<p>Your best bet is the " + "<i style = \"color: #006AFF\">" + str(numbercruncher.give_weighted_job(jobDict)) + "</i>" + " job" + "</p>"
     s += "All the occupations are: " + numbercruncher.get_all_occupations(jobDict)
     return s
 
